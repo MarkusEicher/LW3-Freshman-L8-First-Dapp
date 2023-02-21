a28a054f28d27f8937f13db2442920e1d66cdaab
MarkusEicher <markuseicher@outlook.com>
2023-02-21 01:12:44 +0100

    Compiled and deployed contract add script ref

--- Mood.sol
+++ Mood.sol
@@ -1,3 +1,19 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.4;
 
+contract MoodDiary{
+  // This is the contract's body, here you'll specify the logic for this contract.
+
+  string mood;
+
+  // Sets the mood
+  function setMood(string memory _mood) public {
+    mood = _mood;
+  }
+
+  function getMood() public view returns(string memory) {
+    return mood;
+  }
+
+
+}
